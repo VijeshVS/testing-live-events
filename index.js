3cde4c8845db5362a187219d37ee689bbce1ecc5
Vijesh Shetty <84760221+VijeshVS@users.noreply.github.com>
2026-06-19 16:28:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // API Key Middleware
 app.use((req, res, next) => {
